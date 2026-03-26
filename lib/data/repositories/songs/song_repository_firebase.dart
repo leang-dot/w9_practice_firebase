fc73aa8b0f18492d848f2c401c85b856887fcce5
leang-dot <ounleang180@gmail.com>
2026-03-26 14:56:08 +0700

    W9-01 – Fetch and display songs in the library scree

--- lib/data/repositories/songs/song_repository_firebase.dart
+++ lib/data/repositories/songs/song_repository_firebase.dart
@@ -7,22 +7,38 @@ import '../../dtos/song_dto.dart';
 import 'song_repository.dart';
 
 class SongRepositoryFirebase extends SongRepository {
-  final Uri songsUri = Uri.https('YOUR FIREBASE URL', '/songs.json');
+  final Uri songsUri = Uri.https(
+    'w9-practice-cfaaa-default-rtdb.firebaseio.com',
+    '/songs.json',
+  );
+
+
+
 
   @override
   Future<List<Song>> fetchSongs() async {
     final http.Response response = await http.get(songsUri);
 
-    if (response.statusCode == 200) {
-      // 1 - Send the retrieved list of songs
-      List<dynamic> songJson = json.decode(response.body);
-      return songJson.map((item) => SongDto.fromJson(item)).toList();
-    } else {
-      // 2- Throw expcetion if any issue
-      throw Exception('Failed to load posts');
+    if(response.statusCode == 200){
+      Map<String, dynamic> songsMap = json.decode(response.body);
+
+      return songsMap.entries.map((song){
+        String id = song.key;
+        return SongDto.fromJson(id ,{
+          ...song.value,
+        });
+      }).toList();
+    }else{
+      throw Exception("Failed to load Songs");
     }
   }
 
   @override
-  Future<Song?> fetchSongById(String id) async {}
-}
+  Future<Song?> fetchSongById(String id) async {
+    final songs = await fetchSongs();
+    try {
+      return songs.firstWhere((song) => song.id == id);
+    } catch (e) {
+      return null;
+    }
+  }
--- lib/model/songs/song.dart
+++ lib/model/songs/song.dart
@@ -1,18 +1,20 @@
 class Song {
   final String id;
   final String title;
-  final String artist;
+  final String artistId;
+  final Uri imageUrl;
   final Duration duration;
 
   Song({
     required this.id,
     required this.title,
-    required this.artist,
+    required this.artistId,
+    required this.imageUrl,
     required this.duration,
   });
 
   @override
   String toString() {
-    return 'Song(id: $id, title: $title, artist: $artist, duration: $duration)';
+    return 'Song(id: $id, title: $title, artist: $artistId, duration: $duration)';
   }
 }
